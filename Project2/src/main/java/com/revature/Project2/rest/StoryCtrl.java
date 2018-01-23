50c8358651806a7ad2bb57ae0f282038f8657a63
Patrick Kennedy <patrickjk94@gmail.com>
2018-01-23 01:02:50 -0500

    Added cross origin to all controllers so we can connect while on local host to Angular.

--- Project2/src/main/java/com/revature/Project2/rest/StoryCtrl.java
+++ Project2/src/main/java/com/revature/Project2/rest/StoryCtrl.java
@@ -3,6 +3,7 @@ package com.revature.Project2.rest;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -13,6 +14,7 @@ import com.revature.Project2.beans.Story;
 import com.revature.Project2.service.StoryService;
 
 @RestController
+@CrossOrigin(origins = "*")
 public class StoryCtrl {
 	
 	@Autowired
--- Project2/src/main/java/com/revature/Project2/rest/TaskCtrl.java
+++ Project2/src/main/java/com/revature/Project2/rest/TaskCtrl.java
@@ -3,6 +3,7 @@ package com.revature.Project2.rest;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -13,6 +14,7 @@ import com.revature.Project2.beans.Task;
 import com.revature.Project2.service.TaskService;
 
 @RestController
+@CrossOrigin(origins = "*")
 public class TaskCtrl {
 
 	@Autowired

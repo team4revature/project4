508d9a075e49e6efb772ea17df21a00c8e349331
Destinatus <destinatus@live.com>
2018-01-29 18:28:27 -0500

    member master

--- Project2/src/main/java/com/revature/Project2/service/BoardService.java
+++ Project2/src/main/java/com/revature/Project2/service/BoardService.java
@@ -1,5 +1,7 @@
 package com.revature.Project2.service;
 
+import java.util.ArrayList;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
@@ -13,8 +15,33 @@ public class BoardService {
 	@Autowired
 	BoardRepo boardRepo;
 	
+	public ArrayList<Board> getAllMasterBoards(int id){
+		ArrayList<Board> ab = new ArrayList<Board>();
+		ArrayList<Board> ret = new ArrayList<Board>();
+		ab=(ArrayList<Board>) boardRepo.findAll();
+		for(int i = 0;i< ab.size();i++) {
+			if(ab.get(i).getScrumMaster().getUid() == id) {
+				ret.add(ab.get(i));
+			}
+		}
+		return ret;
+	}
+	
+	public ArrayList<Board> getAllMemberBoards(int id){
+		ArrayList<Board> ab = new ArrayList<Board>();
+		ArrayList<Board> ret = new ArrayList<Board>();
+		ab=(ArrayList<Board>) boardRepo.findAll();
+		for(int i = 0;i< ab.size();i++) {
+			for(int j = 0; j< ab.get(i).getScrumTeam().size();j++) {
+				if(ab.get(i).getScrumTeam().get(j).getUid() ==id) {
+					ret.add(ab.get(i));
+				}
+			}
+		}
+		return ret;
+	}
 	public Board getBoard(int id) {
-		
+		boardRepo.findAll();
 		return boardRepo.findOne(id);
 	}
 	
